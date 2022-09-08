5d21627975261a532e1ea727c9cd6a41fab8626c
kelvin Onumajuru <kelvinonumajuru@gmail.com>
2022-09-08 10:24:07 -0700

    programming is like building

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/* *
+ 
+ * main - to create some texts
+ 
+ * Return:return zero
+ 
+ */
+
+int main(void)
+
+{
+        /* print out some text */
+	puts("\"programming is like building a multingual puzzle");
+	return (0);
+
+}
+	

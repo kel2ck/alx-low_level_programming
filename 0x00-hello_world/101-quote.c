bcee8b75478372768bc748b23a5d6b2cd61cb4ce
kelvin Onumajuru <kelvinonumajuru@gmail.com>
2022-09-08 15:52:51 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,8 @@
 #include <unistd.h>
 
 /**
- * main - prints exactly "and that piece of art is useful"
- *  
- *  Return: Always 0 (Success)
+ * main - prints exactly "and that piece of art is useful" 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {

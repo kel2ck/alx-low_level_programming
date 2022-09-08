18b9044f966d21ed1ef699114390eb3d7ebd7311
kelvin Onumajuru <kelvinonumajuru@gmail.com>
2022-09-08 15:57:18 -0700

    update fix betty code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * main - prints exactly "and that piece of art is useful" 
+ * main - Function to print text using putchar 
  * Return: Always 0 (Success)
  */
 int main(void)

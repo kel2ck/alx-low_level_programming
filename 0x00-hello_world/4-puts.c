12e9921d95c7293954d8998c2edf45b3e917b1a3
kelvin Onumajuru <kelvinonumajuru@gmail.com>
2022-09-08 10:45:23 -0700

    update hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 /**
- * main - to create some texts
- * Return:return zero
+ * main -  to create some texts
+ * Return: return zero
  */
 int main(void)
 {
         /* print out some text */
-	puts("\"programming is like building a multingual puzzle");
+	puts("\"Programming is like building a multingual puzzle");
 	return (0);
 }
 	

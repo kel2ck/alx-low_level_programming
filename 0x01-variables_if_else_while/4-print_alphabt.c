7d5494a0110e26a834ca35cb2916ae9c254d3768
kelvin Onumajuru <kelvinonumajuru@gmail.com>
2022-09-09 11:05:11 -0700

    update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,6 @@
 int main(void)
 {
 	char alphabet;
-	
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		if (alphabet == 'q')
@@ -21,6 +20,5 @@ int main(void)
 		putchar (alphabet);
 	}
 	putchar ('\n');
-
 	return (0);
 }

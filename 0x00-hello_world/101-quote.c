6d5331f3a75d1347941c5c1c914e194b6f03ffe6
kelvin Onumajuru <kelvinonumajuru@gmail.com>
2022-09-08 17:07:10 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,17 @@
-#include <stdio.h>
-#include <unistd.h>
+#include<stdio.h>
 
 /**
- * main - prints exactly "and that piece of art
- * is useful" - Dora Korpar, 2015-10-19",
- * followed by a new line, to the standard error.
- * Return: Always 0 (Success)
+ * main - Entry Point
+ *
+ * Description: prints to the stderr, "and that piece of art
+ * is useful\" - Dora Korpar, 2015-10-19"
+ * 
+ * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" 
-	- Dora Korpar, 2015-10-19\n", 59);
+	fprintf(	
+	stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	);
 	return (1);
 }

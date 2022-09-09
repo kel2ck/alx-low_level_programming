cc6d85c0c55687cd5d5c527be322293ac49c38d0
kelvin Onumajuru <kelvinonumajuru@gmail.com>
2022-09-09 15:43:03 -0700

    combination of two digit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print all possible combination of two digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = i; k <= 9; k++)
+			{
+				for (l = j + 1; l <= 9; l++)
+				{
+					putchar(i % 10 + '0');
+					putchar(j % 10 + '0');
+					putchar(' ');
+					putchar(k % 10 + '0');
+					putchar(l % 10 + '0');
+					if ((j == 9) && (i == 8) && (k == 9) && (1 == 9)
+					{
+						break;
+					}
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

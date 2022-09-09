53ef1f0833ca0fe14ee6ee4df4f77becfa8171be
kelvin Onumajuru <kelvinonumajuru@gmail.com>
2022-09-08 17:16:37 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,12 @@
  * main - Entry Point
  *
  * Description: prints to the stderr, "and that piece of art
- * is useful\" - Dora Korpar, 2015-10-19"
- * 
+ * is useful\" - Dora Korpar, 2015-10-19" 
  * Return: 1
  */
 int main(void)
 {
-	fprintf(	
+	fprintf(
 	stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 	);
 	return (1);

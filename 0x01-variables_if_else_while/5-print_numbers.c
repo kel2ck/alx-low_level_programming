f0c9277e69df4b9e12061c2c03b7fa22a22438e6
kelvin Onumajuru <kelvinonumajuru@gmail.com>
2022-09-09 11:33:47 -0700

    single digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints all single digit numbers of base 10
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+
+	printf("\n");
+	return (0);
+}
+

d498235fa7e7ef9b07a7779f446ce0415615d0ad
kelvin Onumajuru <kelvinonumajuru@gmail.com>
2022-09-13 13:33:14 -0700

    compute absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Function to return absolute value
+ * @n: integer to be checked
+ * Return: integer
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+}

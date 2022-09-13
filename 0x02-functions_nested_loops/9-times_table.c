2ad8dc16e7ac41e4d1a50a70f885cbccab06b6f8
kelvin Onumajuru <kelvinonumajuru@gmail.com>
2022-09-13 14:42:29 -0700

    print 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * times_table - Function to return absolute values.
+ *Return: 0
+ */
+void times_table(void)
+{
+	int digit1;
+	int digit2;
+
+	for (digit1 = 1; digit1 <= 9; digit2++)
+	{
+		_putchar('0');
+		for (digit2 = 1; digit2 <= 9; digit2++)
+		{
+			int result = digit1 * digit2;
+
+			_putchar(',');
+			_putchar(' ');
+			_putchar('0' + (result / 10));
+			_putchar('0' + (result % 10));
+		}
+		_putchar('\n');
+	}
+}

20cd928e19f09c97dccaca0531f102b16e0dc488
kelvin Onumajuru <kelvinonumajuru@gmail.com>
2022-09-13 10:24:03 -0700

    print a file putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: A program that prints _putchar
+ * Return 0 (success)
+ */
+int main(void)
+{
+	char text[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+	return (0);
+}

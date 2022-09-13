333561fa362ac414e86ea2be85543c1840578946
kelvin Onumajuru <kelvinonumajuru@gmail.com>
2022-09-13 10:40:28 -0700

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,12 @@
  */
 int main(void)
 {
-	char text[8] = "_putchar";
-	
-	int i;
+	char *main = "_putchar";
 
-	for (i = 0; i < 8; i++)
+	while (*main)
 	{
-		_putchar(text[i]);
+		_putchar(*main);
+		main++;
 	}
 	_putchar('\n');
 	return (0);

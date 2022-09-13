813a1f2a617404508afb97f3ecc5987e41a60106
kelvin Onumajuru <kelvinonumajuru@gmail.com>
2022-09-13 13:57:14 -0700

    print the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,10 +6,10 @@
  */
 int print_last_digit(int n)
 {
-	int digit = i % 10;
+	int digit = n % 10;
 
-	if (i < 0)
+	if (n < 0)
 		digit = digit * -1;
 	_putchar(digit + '0');
 	return (digit);
-}	
+}

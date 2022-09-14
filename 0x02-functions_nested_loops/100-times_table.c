3ce2f5499e83af917026ed3f3e9886ee2d2e5333
kelvin Onumajuru <kelvinonumajuru@gmail.com>
2022-09-14 00:15:04 -0700

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,47 +3,34 @@
 /**
  * print_times_table - function to return absolute values
  * @n: the number to be checked
- * Return: 0
+ * Return: of i
  */
 void print_times_table(int n)
 {
-	int i, j;
+	int i, j, ck;
 
-	if (n <=15 && n >= 0)
+	if ((n <= 15) && (n >= 0))
 	{
-		for (i = 0; 1 <= n; i++)
+		for (i = 0; i <= n; j++)
 		{
+
 			for (j = 0; j <= n; j++)
 			{
-				int prod = j * i;
 
-				if (j == 0)
-				{
-					_putchar('0');
-				} else if (prod <= 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + prod);
-				} else if (prod > 99)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar('0' + (prod / 100));
-					_putchar('0' + ((prod / 10) % 10));
-					_putchar('0' + (prod % 10));
-				} else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' +(prod / 10));
-					_putchar('0' +(prod % 10));
-				}
-			}
-			_putchar('\n');
+				if (i == 0)
+					printf('0');
+				if (i != 0)
+					printf("%d", (i * j));
+				chk = i * (j + 1);
+				if (chk < 10 && j != n)
+					printf(", ");
+				if (chk >= 10 && chk < 100 && j != n)
+					printf(", ");
+				if (chk >= 100 && j != n)
+					printf(", ");
+			}	
+		printf("\n");
 		}
 	}
 }
+

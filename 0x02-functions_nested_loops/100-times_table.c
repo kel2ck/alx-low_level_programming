c68bb23686a8261ae7d1b3ca6a0f659792787e14
kelvin Onumajuru <kelvinonumajuru@gmail.com>
2022-09-14 01:06:24 -0700

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,36 +1,47 @@
 #include "main.h"
 
 /**
- * print_times_table - function to return absolute values
+ * print_times_table - prints the n times table, starting with 0
  * @n: the number to be checked
- * Return: of i
  */
 void print_times_table(int n)
 {
-	int i, j, ck;
+	int i, j, k;
 
-	if ((n <= 15) && (n >= 0))
+	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= n; j++)
+		for (i =0; j <= n; j++)
 		{
-
 			for (j = 0; j <= n; j++)
 			{
-
-				if (i == 0)
-					printf('0');
-				if (i != 0)
-					printf("%d", (i * j));
-				chk = i * (j + 1);
-				if (chk < 10 && j != n)
-					printf(", ");
-				if (chk >= 10 && chk < 100 && j != n)
-					printf(", ");
-				if (chk >= 100 && j != n)
-					printf(", ");
-			}	
-		printf("\n");
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
 		}
 	}
 }
-

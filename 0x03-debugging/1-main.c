43166bbe22d18cc38184d1ae1f0476c234b90e72
kelvin Onumajuru <kelvinonumajuru@gmail.com>
2022-09-14 23:43:47 -0700

    infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,14 +12,13 @@ int main(void)
 	printf("Infinite lool incoming :(\n");
 
 	i = 0;
-	/**
-	 * while (i < 10)
-	 * {
-	 * 	   putchar(i);
-	 *
-	 * }
-	 */
+
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+
 	printf("infinte loop avoided! \\o/\n");
 
 	return (0);
-}	
+}

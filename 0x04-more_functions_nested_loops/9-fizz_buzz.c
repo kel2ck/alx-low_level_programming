7cb8648c2024bf5f65fe28eccd7d4cf73149704a
kelvin Onumajuru <kelvinonumajuru@gmail.com>
2022-09-15 11:07:08 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the numbers from 1 to 100
+ * print Fizz for multiples of 3
+ * print Buzz for multiples of 5
+ * print FizzBuzz for multiples of both
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 1;
+	printf("%d", a);
+	for (a = 2; a <= 100; a++)
+	{
+		if ((a % 3 == 0) && (a % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+	printf("\n");
+	return (0);
+}

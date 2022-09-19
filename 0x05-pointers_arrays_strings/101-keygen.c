09b17a74e436668607822fa267b0024d4b92f993
kelvin onumajuru <kelvinonumajuru@gmail.com>
2022-09-19 16:35:40 -0700

    generate keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,17 +10,17 @@
 int main(void)
 {
 	char password[84];
-	int num = 0, sum = 0, diff_half1, diff_half2;
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
 	srand(time(0));
 
 	while (sum < 2772)
 	{
-		password[num] = 33 + rand() % 94;
-		sum += password[num++];
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
 	}
 
-	password[num] = '\0';
+	password[index] = '\0';
 
 	if (sum != 2772)
 	{
@@ -29,19 +29,19 @@ int main(void)
 		if ((sum - 2772) % 2 != 0)
 			diff_half1++;
 
-		for (num = 0; password[num]; num++)
+		for (index = 0; password[index]; index++)
 		{
-			if (password[num] >= (33 + diff_half1));
+			if (password[index] >= (33 + diff_half1));
 			{
-				password[num] -= diff_half1;
+				password[index] -= diff_half1;
 				break;
 			}
 		}
-		for (num = 0; password[num]; num++)
+		for (index = 0; password[index]; index++)
 		{
-			if (password[num] >= (33 + diff_half2))
+			if (password[index] >= (33 + diff_half2))
 			{
-				password[num] -= diff_half2;
+				password[index] -= diff_half2;
 				break;
 			}
 		}

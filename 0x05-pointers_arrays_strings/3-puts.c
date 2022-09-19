70d5475476e78ed47e8ded50a44d781ea6d8a06a
kelvin onumajuru <kelvinonumajuru@gmail.com>
2022-09-19 08:46:46 -0700

    print a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,4 @@
-#include <string.h>
-#include <stdlib.h>
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts - A function that prints a string
@@ -11,12 +8,12 @@
 
 void _puts(char *str)
 {
-	int a = 0;
+	int i = 0;
 
-	while (str[a] != '\0')
+	while (str[i] != '\0')
 	{
-		_putchar(str[a]);
-		a++;
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }

1b23ec336259b58687397968d98057a6bcb3b65d
kelvin onumajuru <kelvinonumajuru@gmail.com>
2022-09-19 08:26:00 -0700

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - A function that prints a string
+ * @str: string to be printed
+ * Return: 0 (Success)
+ */
+
+void _puts(char *str)
+{
+	int a;
+	int length = strlen(str);
+
+	for (a = 0; a < length; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}

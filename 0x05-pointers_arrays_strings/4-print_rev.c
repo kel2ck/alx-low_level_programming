87885ec6b239948321f000bc56b4035d08d6cb9a
kelvin onumajuru <kelvinonumajuru@gmail.com>
2022-09-19 09:30:41 -0700

    string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - A function to return absolute value
+ * @str: string value
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int rev = 0;
+
+	while (s[rev] != '\0')
+	{
+		rev++;
+	}
+
+	rev--;
+
+	while (rev >= 0)
+	{
+		_putchar(s[rev]);
+
+		rev--;
+	}
+
+	_putchar('\n');
+}

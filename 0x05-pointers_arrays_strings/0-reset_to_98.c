f684672e6f2c99397b2528eaf95192f49301ff87
kelvin onumajuru <kelvinonumajuru@gmail.com>
2022-09-19 07:28:56 -0700

    98 battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - A function that takes a pointer and updates the value
+ * @n: input value
+ * Return: 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

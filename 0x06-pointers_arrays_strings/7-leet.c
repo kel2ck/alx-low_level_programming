3d297036851948644fc87e996c8c56cd7b81e189
kelvin onumajuru <kelvinonumajuru@gmail.com>
2022-09-22 01:17:27 -0700

    encode a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,14 +1,16 @@
-#include "main."
+#include "main.h"
 
 /**
  * leet - A function that encodes a string into 1337.
- * @str: string to be encoded
+ * @i: string to be encoded
  * Return: Always 0
  */
 
 char *leet(char *i)
 {
-	int a = 0; b, l = 5;
+	int a = 0;
+	int b = 0;
+	int l = 5;
 	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
 	char trw[5] = {'4', '3', '0', '7', 'l'}
 

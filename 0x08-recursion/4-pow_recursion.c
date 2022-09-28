18331a25983efd2215d93fc507db22f012829a9b
kelvin onumajuru <kelvinonumajuru@gmail.com>
2022-09-28 08:12:34 -0700

    raised to a power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - A function that returns the value of x to the power of y.
+ * @x: variable to be calculated
+ * @y: power to be raised
+ * Return: 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int result = x;
+
+	if (y < 0)
+		return (-1);
+
+	else if (y == 0)
+		return (1);
+}

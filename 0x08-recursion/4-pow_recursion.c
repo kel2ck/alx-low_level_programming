a974788d34803af16608fa888a8728b612071c13
kelvin onumajuru <kelvinonumajuru@gmail.com>
2022-09-28 08:20:29 -0700

    raised to a power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,11 +10,9 @@
 
 int _pow_recursion(int x, int y)
 {
-	int result = x;
-
+	if (y == 0)
+		return (1);
 	if (y < 0)
 		return (-1);
-
-	else if (y == 0)
-		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }

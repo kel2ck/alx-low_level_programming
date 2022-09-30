a53e93f4addd512801b03a319cb77513af73c05c
kelvin onumajuru <kelvinonumajuru@gmail.com>
2022-09-30 09:12:44 -0700

    multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,11 +11,14 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	if (argc <= 2)
 	{
-		printf("Error\n")
-			return (1);
+		printf("Error\n");
+		return (1);
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	if (argc > 2)
+	{
+		printf("%i\n", atoi(argv[1] * atoi(argv[2]));
+	}
 	return (0);
 }

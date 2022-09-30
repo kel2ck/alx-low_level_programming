95fdcc8a5fe5b051019ceee680bd92088a08d277
kelvin onumajuru <kelvinonumajuru@gmail.com>
2022-09-30 08:39:47 -0700

    print all arguments received

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A program that prints all arguments it receives.
+ * @argc: number of command line arguments
+ * @argv: Argument values
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	while (agrc--)
+		printf("%s\n", *argv++);
+	return (0);
+}

7f392c6471d090e7f04fabc5bfb2edcc4e964209
kelvin onumajuru <kelvinonumajuru@gmail.com>
2022-10-12 08:15:13 -0700

    print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - A function that prints a name.
+ * @name: input name
+ * @f: Pointer to function
+ *
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}

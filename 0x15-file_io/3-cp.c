fd3eecc37f174d529fc9d498eccd82cce5f7d6d8
kelvin onumajuru <kelvinonumajuru@gmail.com>
2022-10-31 09:56:07 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,7 +21,7 @@ char *create_buffer(char *file)
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", file);
+			"Error: Can't write to %s\n", file);
 		exit(99);
 	}
 

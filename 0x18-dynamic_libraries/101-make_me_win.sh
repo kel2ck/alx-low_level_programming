c63308d0e62f79fe6f9db9e4ddf0e28cfa21bb29
kelvin onumajuru <kelvinonumajuru@gmail.com>
2022-12-14 10:07:19 -0800

    code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/winwin.so https://github.com/kel2ck/alx-low_level_programming/raw/master/0x18-dynamic_libraries/winwin.so
-export LD_PRELOAD=/tmp/winwin.so
+wget -q -O /tmp/winwin.so https://github.com/kel2ck/alx-low_level_programming/raw/master/0x18-dynamic_libraries/all_winwin.so
+export LD_PRELOAD=/tmp/all_winwin.so
